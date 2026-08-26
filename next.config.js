307e94ac0f1d116beea4b780abfc9718ea826418
blinengah <blinengah18@gmail.com>
2026-08-26 14:55:56 +0700

    Add initial Next.js configuration file

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/**
+ * ============================================================
+ * APPLICATION : Portal Sinergi Operasional BUJP-User
+ * COMPONENT   : next.config.js
+ * CREATED BY  : Bli Nengah
+ * VERSION     : v1.0.0
+ * YEAR        : 2026
+ * ============================================================
+ */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
